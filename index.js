b6b6e7aacd5cf100cc1eb22c6201ce59cdb80e1e
Salim KAYABASI <salim.kayabasi@gmail.com>
2018-03-21 15:11:35 +0100

    adding boilerplate templates

--- index.js
+++ index.js
@@ -1,50 +1,55 @@
 const _ = require('lodash');
 
+const defaultConfig = {
+  separator: ' ',
+};
+
 const createDataOverlays = (items, config) => {
-    const messages = [];
-    const overlay = {};
-    _.chain(items)
-        .compact()
-        .forEach((item) => {
-            if (_.isObject(item)) {
-                const result = item instanceof Error
-                    ? { stack: item.stack }
-                    : item;
-                _.assignIn(overlay, result);
-            } else {
-                messages.push(item);
-            }
-        })
-        .value();
+  const messages = [];
+  const overlay = {};
+  _.chain(items)
+    .compact()
+    .forEach((item) => {
+      if (_.isObject(item)) {
+        const result = item instanceof Error
+          ? { stack: item.stack }
+          : item;
+        _.assignIn(overlay, result);
+      } else {
+        messages.push(item);
+      }
+    })
+    .value();
 
-    if (messages.length) {
-        overlay.msg = messages.join(config.separator);
-    }
-    return overlay;
-}
+  if (messages.length) {
+    overlay.msg = messages.join(config.separator);
+  }
+  return overlay;
+};
+
 const formatter = (event, config) => {
-    const output = {
-        ts: event.startTime,
-        level: event.level.levelStr,
-    };
+  const output = {
+    ts: event.startTime,
+    level: event.level.levelStr,
+  };
 
-    if (event.categoryName !== 'default') {
-        output.category = event.categoryName;
-    }
+  if (event.categoryName !== 'default') {
+    output.category = event.categoryName;
+  }
 
-    const messages = event.data;
-    if (!_.isEmpty(messages)) {
-        Object.assign(output, createDataOverlays(messages, config));
-    }
-    return output;
-}
+  const messages = event.data;
+  if (!_.isEmpty(messages)) {
+    Object.assign(output, createDataOverlays(messages, config));
+  }
+  return output;
+};
 
 const jsonLayout = (config) => {
-    config.separator = config.separator || ' ';
-    return (event) => {
-        const result = formatter(event, config);
-        return JSON.stringify(result, null, config.space || 0);
-    }
-}
+  const options = _.assignIn(defaultConfig, config);
+  return (event) => {
+    const result = formatter(event, options);
+    return JSON.stringify(result, null, options.space);
+  };
+};
 
 module.exports = jsonLayout;
