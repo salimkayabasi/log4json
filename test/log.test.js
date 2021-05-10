9030931adac6eddc5814f2830b766e172db4a9c4
Salim KAYABASI <salim.kayabasi@gmail.com>
2021-05-10 12:17:42 +0200

    test: using log4js instance for testing flow

--- test/log.test.js
+++ test/log.test.js
@@ -1,77 +1,133 @@
-const jsonLayout = require('../index');
+const { addLayout, configure, getLogger } = require('log4js');
+const layout = require('../index');
 
-const eventGeneratorWithCategory = (categoryName, ...values) => ({
-  startTime: 'startTime',
-  level: {
-    levelStr: 'mock',
-  },
-  categoryName,
-  data: values,
-});
-
-const eventGenerator = (...values) => eventGeneratorWithCategory('default', ...values);
+addLayout('json', layout);
+const setUpTest = (config = {}) => {
+  configure({
+    appenders: {
+      log4json: {
+        type: 'stdout',
+        layout: {
+          ...config,
+          type: 'json',
+        },
+      },
+    },
+    categories: {
+      default: {
+        appenders: ['log4json'],
+        level: 'all',
+      },
+    },
+  });
+};
 
 describe('log4json', () => {
-  test('should return string', () => {
-    const logger = jsonLayout();
-    const result = logger(eventGenerator('test'));
-    expect(typeof result).toBe('string');
+  let logger = getLogger('log4json');
+  const startTime = '2020-01-01T01:00:00.000Z';
+  // eslint-disable-next-line no-underscore-dangle,no-console
+  const spyConsole = jest.spyOn(console._stdout, 'write').mockImplementation(() => {
   });
+
+  beforeAll(() => {
+    jest.useFakeTimers('modern');
+    jest.setSystemTime(1577840400000);
+  });
+
+  beforeEach(() => {
+    spyConsole.mockClear();
+  });
+
+  afterAll(() => {
+    spyConsole.mockRestore();
+    jest.useRealTimers();
+  });
+
+  test('should return stringified JSON', () => {
+    setUpTest();
+    logger = getLogger();
+    logger.info('do some log');
+    const log = spyConsole.mock.calls[0][0];
+    expect(typeof log).toBe('string');
+    expect(() => {
+      JSON.parse(log);
+    }).not.toThrowError();
+  });
+
   test('should return default props if no args', () => {
-    const logger = jsonLayout();
-    const result = JSON.parse(logger(eventGenerator()));
+    setUpTest();
+    logger = getLogger();
+    logger.debug();
+    const result = JSON.parse(spyConsole.mock.calls[0][0]);
     expect(result).not.toHaveProperty('message');
     expect(result).not.toHaveProperty('category');
-    expect(result).toHaveProperty('ts', 'startTime');
-    expect(result).toHaveProperty('level', 'mock');
+    expect(result).toHaveProperty('ts', startTime);
+    expect(result).toHaveProperty('level', 'DEBUG');
   });
-  test('should have message ts and level if no category', () => {
-    const logger = jsonLayout();
-    const result = JSON.parse(logger(eventGenerator('test')));
-    expect(result).not.toHaveProperty('category');
-  });
+
   test('should have category if is defined', () => {
-    const logger = jsonLayout();
-    const result = JSON.parse(logger(eventGeneratorWithCategory('test-category', 'test')));
+    setUpTest();
+    logger = getLogger('test-category');
+    logger.debug();
+    const result = JSON.parse(spyConsole.mock.calls[0][0]);
     expect(result).toHaveProperty('category', 'test-category');
   });
+
   test('should concat all string messages', () => {
-    const logger = jsonLayout();
-    const result = JSON.parse(logger(eventGenerator('concat', 'this message', 'with this one')));
+    setUpTest();
+    logger = getLogger();
+    logger.debug('concat', 'this message', 'with this one');
+    const result = JSON.parse(spyConsole.mock.calls[0][0]);
     expect(result).toHaveProperty('message', 'concat this message with this one');
   });
+
   test('should assign all props in to output', () => {
-    const logger = jsonLayout();
-    const result = JSON.parse(logger(eventGenerator('extend', { a: 1 }, { b: 2 }, { c: 3 })));
+    setUpTest();
+    logger = getLogger();
+    logger.debug('extend', { a: 1 }, { b: 2 }, { c: 3 });
+    const result = JSON.parse(spyConsole.mock.calls[0][0]);
     expect(result).toHaveProperty('a', 1);
     expect(result).toHaveProperty('b', 2);
     expect(result).toHaveProperty('c', 3);
   });
+
   test('should return the stack of error', () => {
-    const logger = jsonLayout();
-    const result = JSON.parse(logger(eventGenerator('error', new Error('test'))));
+    setUpTest();
+    logger = getLogger();
+    logger.debug('error', new Error('test'));
+    const result = JSON.parse(spyConsole.mock.calls[0][0]);
     expect(result).toHaveProperty('stack');
   });
+
   test('should concat the message with given separator', () => {
-    const logger = jsonLayout({ separator: '.$.' });
-    const result = JSON.parse(logger(eventGenerator('concat with', 'this message', 'with this one')));
+    setUpTest({ separator: '.$.' });
+    logger = getLogger();
+    logger.debug('concat with', 'this message', 'with this one');
+    const result = JSON.parse(spyConsole.mock.calls[0][0]);
     expect(result).toHaveProperty('message', 'concat with.$.this message.$.with this one');
   });
+
   test('should format the message with given space', () => {
     [null, 1, 2, 3, 4]
-      .forEach((space) => {
-        const logger = jsonLayout({ space });
-        const result = logger(eventGenerator('space'));
-        expect(result).toBe(JSON.stringify(JSON.parse(result), null, space));
+      .forEach((space, index) => {
+        setUpTest({ space });
+        logger = getLogger();
+        logger.info('space');
+        const result = spyConsole.mock.calls[index][0];
+        expect(result).toBe(`${JSON.stringify(JSON.parse(result), null, space)}\n`);
       });
   });
+
   test('should omit invalid data', () => {
-    const logger = jsonLayout();
-    const result = JSON.parse(logger(eventGenerator('valid', undefined, null)));
+    setUpTest();
+    logger = getLogger('test-category');
+    logger.debug('valid', undefined, null);
+    const result = JSON.parse(spyConsole.mock.calls[0][0]);
     expect(result).toHaveProperty('message', 'valid');
   });
+
   test('should change the pre-reserved keys if the config has', () => {
-    const logger = jsonLayout({
+    setUpTest({
       props: {
         ts: '@timestamp',
         level: 'lvl',
@@ -80,13 +136,15 @@ describe('log4json', () => {
         message: 'context',
       },
     });
-    const result = JSON.parse(logger(eventGeneratorWithCategory('my-category', 'custom props', new Error('stack'))));
+    logger = getLogger('my-category');
+    logger.error('custom props', new Error('stack'));
+    const result = JSON.parse(spyConsole.mock.calls[0][0]);
 
     expect(result).not.toHaveProperty('ts');
-    expect(result).toHaveProperty('@timestamp', 'startTime');
+    expect(result).toHaveProperty('@timestamp', startTime);
 
     expect(result).not.toHaveProperty('level');
-    expect(result).toHaveProperty('lvl', 'mock');
+    expect(result).toHaveProperty('lvl', 'ERROR');
 
     expect(result).not.toHaveProperty('category');
     expect(result).toHaveProperty('cat', 'my-category');
