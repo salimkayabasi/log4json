656773f1b89b88b3b3e4785124b2a32259a2b4ca
Salim KAYABASI <salim.kayabasi@gmail.com>
2021-05-10 16:34:52 +0200

    refactor: folder structure

---
+++ src/log4json.js
@@ -0,0 +1,71 @@
+/* istanbul ignore next */
+// https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore
+const isEmpty = (obj) => [Object, Array]
+  .includes((obj || {}).constructor) && !Object.entries((obj || {})).length;
+// https://github.com/lodash/lodash/blob/4.17.15/lodash.js#L11743-L11745
+const isObject = (value) => {
+  const type = typeof value;
+  return value != null && (type === 'object' || type === 'function');
+};
+
+const defaultConfig = {
+  separator: ' ',
+  space: null,
+  omitDefaultCategory: true,
+  props: {
+    ts: 'ts',
+    level: 'level',
+    category: 'category',
+    stack: 'stack',
+    message: 'message',
+  },
+};
+
+const transform = (config, items) => {
+  const messages = [];
+  const overlay = {};
+  items.filter(Boolean)
+    .forEach((item) => {
+      if (isObject(item)) {
+        const result = item instanceof Error
+          ? { [config.props.stack]: item.stack }
+          : item;
+        Object.assign(overlay, result);
+      } else {
+        messages.push(item);
+      }
+    });
+
+  if (!isEmpty(messages)) {
+    overlay[config.props.message] = messages.join(config.separator);
+  }
+  return overlay;
+};
+
+const formatter = (event, options) => {
+  const output = {
+    [options.props.ts]: event.startTime,
+    [options.props.level]: event.level.levelStr,
+  };
+
+  if (event.categoryName !== 'default' || !options.omitDefaultCategory) {
+    output[options.props.category] = event.categoryName;
+  }
+  return {
+    ...transform(options, event.data),
+    ...output,
+  };
+};
+
+const jsonLayout = (config) => {
+  const options = {
+    ...defaultConfig,
+    ...config,
+  };
+  return (event) => {
+    const result = formatter(event, options);
+    return JSON.stringify(result, null, options.space);
+  };
+};
+
+module.exports = jsonLayout;
